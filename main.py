bd754d3aedf379faba7d5f8648895dc68c76dc3b
JinPark <jiny199109@gmail.com>
2018-09-17 18:07:42 +0900

    Assignment #2

--- main.py
+++ main.py
@@ -1,34 +1,103 @@
-# Problem 1
-# 2 to 26 array
-M = list(range(2, 27))
-print(M)
-
-# Problem 2
-# as a 5x5
-M = [M[i:i + 5] for i in range(0, len(M), 5)]
-print(M)
-
-# Problem 3
-# make inner value 0
-for i in range(1, 4):
-	for j in range(1, 4):
-		M[i][j] = 0
-print(M)
-
-# Problem 4
-# M^2
-M2 = [[0 for i in range(5)] for j in range(5)]
-for i in range(len(M)):
-	for j in range(len(M[0])):
-		for k in range(len(M)):
-			M2[i][j] += M[i][k] * M[k][j]
-M = M2
-print(M)
-
-# Problem 5
-# magnitude of v in M
-# use np.sqrt
+import glfw
+from OpenGL.GL import *
 import numpy as np
 
-magnitude = np.sqrt(sum([np.square(M[0][i]) for i in range(5)]))
-print(magnitude)
+# Global variable
+gComposedM = np.identity(2)
+
+
+def render(T):
+	glClear(GL_COLOR_BUFFER_BIT)
+	glLoadIdentity()
+	# draw cooridnate
+	glBegin(GL_LINES)
+	glColor3ub(255, 0, 0)
+	glVertex2fv(np.array([0., 0.]))
+	glVertex2fv(np.array([1., 0.]))
+	glColor3ub(0, 255, 0)
+	glVertex2fv(np.array([0., 0.]))
+	glVertex2fv(np.array([0., 1.]))
+	glEnd()
+	# draw triangle
+	glBegin(GL_TRIANGLES)
+	glColor3ub(255, 255, 255)
+	glVertex2fv(T @ np.array([0.0, 0.5]))
+	glVertex2fv(T @ np.array([0.0, 0.0]))
+	glVertex2fv(T @ np.array([0.5, 0.0]))
+	glEnd()
+
+
+def key_callback(window, key, scancode, action, mods):
+	if action != glfw.PRESS: return
+
+	global gComposedM
+	newM = np.identity(2)
+
+	if key == glfw.KEY_W:  # Scale by 0.9 times in x direction
+		newM = np.array([[0.9, 0], [0, 1]])
+
+	elif key == glfw.KEY_E:  # Scale by 1.1 times in x direction
+		newM = np.array([[1.1, 0], [0, 1]])
+
+	elif key == glfw.KEY_S:  # Rotate by 10 degrees counterclockwise
+		th = np.radians(10)
+		newM = np.array([[np.cos(th), -np.sin(th)], [np.sin(th), np.cos(th)]])
+
+	elif key == glfw.KEY_D:  # Rotate by 10 degress clockwise
+		th = np.radians(-10)
+		newM = np.array([[np.cos(th), -np.sin(th)], [np.sin(th), np.cos(th)]])
+
+	elif key == glfw.KEY_X:  # Shear by a factor of -0.1 in x direction
+		newM = np.array([[1, -0.1], [0, 1]])
+
+	elif key == glfw.KEY_C:  # Shear by a factor of 0.1 in x direction
+		newM = np.array([[1, 0.1], [0, 1]])
+
+	elif key == glfw.KEY_R:  # Reflection across x axis
+		newM = np.array([[1, 0], [0, -1]])
+
+	elif key == glfw.KEY_1:  # Reset the triangle with identity matrix
+		gComposedM = np.identity(2)
+
+	else:
+		return
+
+	gComposedM = newM @ gComposedM
+
+	return
+
+
+def main():
+	# Initialize the library
+	if not glfw.init():
+		return
+	# Create a Windowed mode window and its OpenGL context
+	window = glfw.create_window(640, 480, "2017124921 JungJin Park", None, None)
+	if not window:
+		glfw.terminate()
+		return
+
+	# Key callback
+	glfw.set_key_callback(window, key_callback)
+
+	# Make the window's context current
+	glfw.make_context_current(window)
+
+	# let's just skip - we'll see this later
+	glfw.swap_interval(1)
+
+	# Loop until the user closes the window
+	while not glfw.window_should_close(window):
+		# Poll events
+		glfw.poll_events()
+
+		# Swap front and back buffers
+		glfw.swap_buffers(window)
+
+		render(gComposedM)
+
+	glfw.terminate()
+
+
+if __name__ == "__main__":
+	main()
