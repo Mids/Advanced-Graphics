23d907c7ee8c98809c9b491869cadfd01702974f
Jin Park <jiny199109@gmail.com>
2018-10-15 21:16:50 +0900

    4

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# 2017124921 JungJin Park Assignment 3-2
+# 2017124921 JungJin Park Assignment 4
 import glfw
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -8,32 +8,39 @@ import numpy as np
 gCamAng = 0.
 
 
-def drawBox():
-    glBegin(GL_QUADS)
-    glVertex3fv(np.array([1, 1, 0.]))
-    glVertex3fv(np.array([-1, 1, 0.]))
-    glVertex3fv(np.array([-1, -1, 0.]))
-    glVertex3fv(np.array([1, -1, 0.]))
-    glEnd()
+def myLookAt(eye, at, up):  # eye, at, up are 1D numpy array of length 3
+    global gCamAng
+    forward = eye - at
+    forward = forward/np.sqrt(np.dot(forward, forward))
+
+    side = np.cross(up, forward)
+    side = side/np.sqrt(np.dot(side, side))
+
+    up = np.cross(forward, side)
+
+    pos = np.array([-np.dot(eye, side), -np.dot(eye, up), -np.dot(eye, forward)])
+
+    viewMatrix = np.array([[side[0], up[0], forward[0], 0.0],
+                          [side[1], up[1], forward[1], 0.0],
+                          [side[2], up[2], forward[2], 0.0],
+                          [pos[0], pos[1], pos[2], 1.0]])
+
+    glMultMatrixf(viewMatrix)
 
 
-def drawTriangleTransformedBy(M):
-    glBegin(GL_TRIANGLES)
-    glVertex3fv((M @ np.array([.0, .5, 0., 1.]))[:-1])
-    glVertex3fv((M @ np.array([.0, .0, 0., 1.]))[:-1])
-    glVertex3fv((M @ np.array([.5, .0, 0., 1.]))[:-1])
-    glEnd()
+def render(camAng):
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glEnable(GL_DEPTH_TEST)
 
+    glLoadIdentity()
+    glOrtho(-1, 1, -1, 1, -10, 10)
 
-def drawTriangle():
-    glBegin(GL_TRIANGLES)
-    glVertex3fv(np.array([.0, .5, 0.]))
-    glVertex3fv(np.array([.0, .0, 0.]))
-    glVertex3fv(np.array([.5, .0, 0.]))
-    glEnd()
+    # rotate "camera" position (right-multiply the current matrix by viewing matrix)
+    # try to change parameters
+    # gluLookAt(1 * np.sin(camAng), 1, 1 * np.cos(camAng), 0, 0, 0, 0, 1, 0)
+    myLookAt(np.array([1 * np.sin(camAng), 1, 1 * np.cos(camAng)]), np.array([0, 0, 0]), np.array([0, 1, 0]))
 
-
-def drawFrame():
+    # draw coordinates
     glBegin(GL_LINES)
     glColor3ub(255, 0, 0)
     glVertex3fv(np.array([0., 0., 0.]))
@@ -47,122 +54,30 @@ def drawFrame():
     glEnd()
 
 
-def render(camAng, count):
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glEnable(GL_DEPTH_TEST)
-
-    # set the current matrix to the identity matrix
-    glLoadIdentity()
-
-    # use orthogonal projection (multiply the current matrix by "projection" matrix - we'll see details later)
-    glOrtho(-1, 1, -1, 1, -1, 1)
-
-    # rotate "camera" position (multiply the current matrix by "projection" matrix - we'll see details later)
-    gluLookAt(.1 * np.sin(camAng), .1, .1 * np.cos(camAng), 0, 0, 0, 0, 1, 0)
-
-    # draw cooridnate
-    glBegin(GL_LINES)
-    glColor3ub(255, 0, 0)
-    glVertex3fv(np.array([0., 0., 0.]))
-    glVertex3fv(np.array([1., 0., 0.]))
-    glColor3ub(0, 255, 0)
-    glVertex3fv(np.array([0., 0., 0.]))
-    glVertex3fv(np.array([0., 1., 0.]))
-    glColor3ub(0, 0, 255)
-    glVertex3fv(np.array([0., 0., 0.]))
-    glVertex3fv(np.array([0., 0., 1.]))
-    glEnd()
-
-    ###############################################
-    # edit here
-
-    # blue base transformation
-    glPushMatrix()
-    glTranslatef(-.5 + (count % 360) * .003, 0, 0)
-    drawFrame()
-
-    # blue base drawing
-    glPushMatrix()
-    glScalef(.2, .2, .2)
-    glColor3ub(0, 0, 255)
-    drawBox()
-    glPopMatrix()
-
-    # red arm transformation
-    glPushMatrix()
-    glRotatef(count % 360, 0, 0, 1)
-    glTranslatef(.5, 0, .01)
-    drawFrame()
-
-    # red arm drawing
-    glPushMatrix()
-    glScalef(.5, .1, .1)
-    glColor3ub(255, 0, 0)
-    drawBox()
-    glPopMatrix()
-
-    # green arm transformation
-    glPushMatrix()
-    glTranslate(.5, 0, .01)
-    glRotatef(count % 360, 0, 0, 1)
-    drawFrame()
-
-    # green arm drawing
-    glPushMatrix()
-    glScalef(.2, .2, .2)
-    glColor3ub(0, 255, 0)
-    drawBox()
-    glPopMatrix()
-
-    glPopMatrix()
-    glPopMatrix()
-    glPopMatrix()
-
-
 def key_callback(window, key, scancode, action, mods):
     global gCamAng
     # rotate the camera when 1 or 3 key is pressed or repeated
-
     if action == glfw.PRESS or action == glfw.REPEAT:
         if key == glfw.KEY_1:
             gCamAng += np.radians(-10)
         elif key == glfw.KEY_3:
             gCamAng += np.radians(10)
 
-    return
-
 
 def main():
-    # Initialize the library
     if not glfw.init():
         return
-    # Create a Windowed mode window and its OpenGL context
-    window = glfw.create_window(640, 480, "2017124921 JungJin Park", None, None)
+    window = glfw.create_window(640, 640, 'Lecture7', None, None)
     if not window:
         glfw.terminate()
         return
-
-    # Make the window's context current
     glfw.make_context_current(window)
-
-    # Key callback
     glfw.set_key_callback(window, key_callback)
 
-    # let's just skip - we'll see this later
-    glfw.swap_interval(1)
-
-    count = 0
-    # Loop until the user closes the window
     while not glfw.window_should_close(window):
-        # Poll events
         glfw.poll_events()
-
-        render(gCamAng, count)
-
-        # Swap front and back buffers
+        render(gCamAng)
         glfw.swap_buffers(window)
-
-        count += 1
 
     glfw.terminate()
 
