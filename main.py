a71edc55b86ab213fac4e926ee17aacfb4b551bd
Jin Park <jiny199109@gmail.com>
2018-11-16 17:46:59 +0900

    7

--- main.py
+++ main.py
@@ -1,109 +1,180 @@
-# 2017124921 JungJin Park Assignment 5
-
+###############################
+# 2017124921 JungJin Park
 import glfw
 from OpenGL.GL import *
 from OpenGL.GLU import *
 import numpy as np
+from OpenGL.arrays import vbo
+import ctypes
 
 gCamAng = 0.
 gCamHeight = 1.
 
 
 def createVertexAndIndexArrayIndexed():
-    varr = np.array([
-        (0, 0, 0),  # v0
-        (1.5, 0, 0),  # v1
-        (0, 1.5, 0),  # v2
-        (0, 0, 1.5),  # v3
-    ], 'float32')
+	varr = np.array([
+		(-0.5773502691896258, 0.5773502691896258, 0.5773502691896258),  # v0 normal
+		(-1, 1, 1),  # v0 position
+		(0.8164965809277261, 0.4082482904638631, 0.4082482904638631),  # v1 normal
+		(1, 1, 1),  # v1 position
+		(0.4082482904638631, -0.4082482904638631, 0.8164965809277261),  # v2 normal
+		(1, -1, 1),  # v2 position
+		(-0.4082482904638631, -0.8164965809277261, 0.4082482904638631),  # v3 normal
+		(-1, -1, 1),  # v3 position
+		(-0.4082482904638631, 0.4082482904638631, -0.8164965809277261),
+		(-1, 1, -1),  # v4
+		(0.4082482904638631, 0.8164965809277261, -0.4082482904638631),
+		(1, 1, -1),  # v5
+		(0.5773502691896258, -0.5773502691896258, -0.5773502691896258),
+		(1, -1, -1),  # v6
+		(-0.8164965809277261, -0.4082482904638631, -0.4082482904638631),
+		(-1, -1, -1),  # v7
+	], 'float32')
 
-    iarr = np.array([
-        (0, 2, 1),
-        (0, 3, 2),
-        (0, 1, 3),
-        (1, 2, 3),
-    ])
+	iarr = np.array([
+		(0, 2, 1),
+		(0, 3, 2),
+		(4, 5, 6),
+		(4, 6, 7),
+		(0, 1, 5),
+		(0, 5, 4),
+		(3, 6, 2),
+		(3, 7, 6),
+		(1, 2, 6),
+		(1, 6, 5),
+		(0, 7, 3),
+		(0, 4, 7)
+	])
 
-    return varr, iarr
+	return varr, iarr
 
 
-def drawCube_glDrawArrays():
-    global gVertexArrayIndexed, gIndexArray
-    varr = gVertexArrayIndexed
-    iarr = gIndexArray
-    glEnableClientState(GL_VERTEX_ARRAY)  # Enable it to use vertex array
-    glVertexPointer(3, GL_FLOAT, 3 * varr.itemsize, varr)
-    glDrawElements(GL_TRIANGLES, iarr.size, GL_UNSIGNED_INT, iarr)
+def render(ang):
+	global gCamAng, gCamHeight
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+	glEnable(GL_DEPTH_TEST)
+
+	glMatrixMode(GL_PROJECTION)  # use projection matrix stack for projection transformation for correct lighting
+	glLoadIdentity()
+	gluPerspective(45, 1, 1, 10)
+
+	glMatrixMode(GL_MODELVIEW)
+	glLoadIdentity()
+	gluLookAt(5 * np.sin(gCamAng), gCamHeight, 5 * np.cos(gCamAng), 0, 0, 0, 0, 1, 0)
+
+	drawFrame()
+
+	glEnable(GL_LIGHTING)  # try to uncomment: no lighting
+	glEnable(GL_LIGHT0)
+
+	glEnable(GL_RESCALE_NORMAL)  # try to uncomment: lighting will be incorrect if you scale the object
+	# glEnable(GL_NORMALIZE)
+
+	# light position
+	glPushMatrix()
+
+	# glRotatef(ang,0,1,0)  # try to uncomment: rotate light
+	lightPos = (3., 4., 5., 1.)  # try to change 4th element to 0. or 1.
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos)
+
+	glPopMatrix()
+
+	# light intensity for each color channel
+	ambientLightColor = (.1, .1, .1, 1.)
+	diffuseLightColor = (1., 1., 1., 1.)
+	specularLightColor = (1., 1., 1., 1.)
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLightColor)
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLightColor)
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLightColor)
+
+	# material reflectance for each color channel
+	diffuseObjectColor = (1., 0., 0., 1.)
+	specularObjectColor = (1., 0., 0., 1.)
+	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, diffuseObjectColor)
+	# glMaterialfv(GL_FRONT, GL_SPECULAR, specularObjectColor)
+
+	glPushMatrix()
+	# glRotatef(ang,0,1,0)    # try to uncomment: rotate object
+	# glScalef(1.,.2,1.)    # try to uncomment: scale object
+
+	glColor3ub(0, 0, 255)  # glColor*() is ignored if lighting is enabled
+
+	# drawCube_glVertex()
+	drawCube_glDrawArray()
+	glPopMatrix()
+
+	glDisable(GL_LIGHTING)
 
 
-def render():
-    global gCamAng, gCamHeight
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glEnable(GL_DEPTH_TEST)
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
-
-    glLoadIdentity()
-    gluPerspective(45, 1, 1, 10)
-    gluLookAt(5 * np.sin(gCamAng), gCamHeight, 5 * np.cos(gCamAng), 0, 0, 0, 0, 1, 0)
-
-    drawFrame()
-    glColor3ub(255, 255, 255)
-    # drawCube_glVertex()
-    drawCube_glDrawArrays()
+def drawCube_glDrawArray():
+	global gVertexArraySeparate, gIndexArray
+	varr = gVertexArraySeparate
+	iarr = gIndexArray
+	glEnableClientState(GL_VERTEX_ARRAY)
+	glEnableClientState(GL_INDEX_ARRAY)
+	glEnableClientState(GL_NORMAL_ARRAY)
+	glNormalPointer(GL_FLOAT, 6 * varr.itemsize, varr)
+	glVertexPointer(3, GL_FLOAT, 6 * varr.itemsize, ctypes.c_void_p(varr.ctypes.data + 3 * varr.itemsize))
+	glDrawElements(GL_TRIANGLES, iarr.size, GL_UNSIGNED_INT, iarr)
 
 
 def drawFrame():
-    glBegin(GL_LINES)
-    glColor3ub(255, 0, 0)
-    glVertex3fv(np.array([0., 0., 0.]))
-    glVertex3fv(np.array([1., 0., 0.]))
-    glColor3ub(0, 255, 0)
-    glVertex3fv(np.array([0., 0., 0.]))
-    glVertex3fv(np.array([0., 1., 0.]))
-    glColor3ub(0, 0, 255)
-    glVertex3fv(np.array([0., 0., 0]))
-    glVertex3fv(np.array([0., 0., 1.]))
-    glEnd()
+	glBegin(GL_LINES)
+	glColor3ub(255, 0, 0)
+	glVertex3fv(np.array([0., 0., 0.]))
+	glVertex3fv(np.array([1., 0., 0.]))
+	glColor3ub(0, 255, 0)
+	glVertex3fv(np.array([0., 0., 0.]))
+	glVertex3fv(np.array([0., 1., 0.]))
+	glColor3ub(0, 0, 255)
+	glVertex3fv(np.array([0., 0., 0]))
+	glVertex3fv(np.array([0., 0., 1.]))
+	glEnd()
 
 
 def key_callback(window, key, scancode, action, mods):
-    global gCamAng, gCamHeight
-    if action == glfw.PRESS or action == glfw.REPEAT:
-        if key == glfw.KEY_1:
-            gCamAng += np.radians(-10)
-        elif key == glfw.KEY_3:
-            gCamAng += np.radians(10)
-        elif key == glfw.KEY_2:
-            gCamHeight += .1
-        elif key == glfw.KEY_W:
-            gCamHeight += -.1
+	global gCamAng, gCamHeight
+	if action == glfw.PRESS or action == glfw.REPEAT:
+		if key == glfw.KEY_1:
+			gCamAng += np.radians(-10)
+		elif key == glfw.KEY_3:
+			gCamAng += np.radians(10)
+		elif key == glfw.KEY_2:
+			gCamHeight += .1
+		elif key == glfw.KEY_W:
+			gCamHeight += -.1
 
 
-gVertexArrayIndexed = None
+gVertexArraySeparate = None
 gIndexArray = None
 
 
 def main():
-    global gVertexArrayIndexed, gIndexArray
+	global gVertexArraySeparate, gIndexArray
 
-    if not glfw.init():
-        return
-    window = glfw.create_window(640, 640, '2017124921 JungJin Park Assignment 5', None, None)
-    if not window:
-        glfw.terminate()
-        return
-    glfw.make_context_current(window)
-    glfw.set_key_callback(window, key_callback)
+	if not glfw.init():
+		return
+	window = glfw.create_window(640, 640, 'Lecture17', None, None)
+	if not window:
+		glfw.terminate()
+		return
+	glfw.make_context_current(window)
+	glfw.set_key_callback(window, key_callback)
+	glfw.swap_interval(1)
 
-    gVertexArrayIndexed, gIndexArray = createVertexAndIndexArrayIndexed()
+	gVertexArraySeparate, gIndexArray = createVertexAndIndexArrayIndexed()
 
-    while not glfw.window_should_close(window):
-        glfw.poll_events()
-        render()
-        glfw.swap_buffers(window)
+	count = 0
+	while not glfw.window_should_close(window):
+		glfw.poll_events()
+		ang = count % 360
+		render(ang)
+		count += 1
+		glfw.swap_buffers(window)
 
-    glfw.terminate()
+	glfw.terminate()
 
 
 if __name__ == "__main__":
-    main()
+	main()
